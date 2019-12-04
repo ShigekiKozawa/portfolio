e1bb28771e59d6f9986cb2721c47ed3e0ce59329
ShigekiKozawa <sigekikozawa@aol.com>
2019-12-04 17:05:15 +0900

    archiveまで

--- js/common.js
+++ js/common.js
@@ -1,42 +1,10 @@
 $(function(){
-	// $('.section').on({
-	// 	'mouseenter': function(){
-	// 		$(this).addClass('active');
-	// 		$('.section:not(.active)').addClass('hidden');
-	// 	},
-	// 	'mouseleave': function(){
-	// 		$(this).removeClass('active');
-	// 		$('.section:not(.active)').removeClass('hidden');
-	// 	},
-	// });
 	var device = (function() {
 		var ua = navigator.userAgent;
 		if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
 		    return 'sp';
 		}
 	})();
-    // if( device == 'sp' ) {
-    //     // $("#nav-toggle").on('click', function() {
-    //     //     $(this).toggleClass('active');
-    //     //     if ($(this).hasClass('active')) {
-    //     //         $('#nav-cont').addClass('active');
-    //     //     } else {
-    //     //         $('#nav-cont').removeClass('active');
-    //     //     }
-    //     // });
-    //     // $("#nav-cont a").on('click', function() {
-    //     //     $('#nav-toggle, #nav-cont').removeClass('active');
-    //     // });
-    // }else {}
-    // $('.works_list_movie a').on('click', function(){
-    //     var movie = $(this).attr('value');
-    //     console.log(movie);
-    //     $('#modal').addClass('active');
-    //     $('#modal-movie video').attr('src', './img/' + movie + '.mp4');
-    // });
-    // $('#modal-overlay, #close_btn').on('click', function(){
-    //     $('#modal').removeClass('active');
-    // });
     // animation
     $('.anim').waypoint(function(event, direction) {
         $(this).addClass('anim-active')
@@ -70,7 +38,7 @@ $(function(){
 	$(window).on('scroll', function(){
 		var position = $(this).scrollTop();
 		var sect1 = $('#about');
-		var sect2 = $('#achieve');
+		var sect2 = $('#archive');
 		var sect3 = $('#contact');
 		var sect1_h = sect1.offset().top;
 		var sect2_h = sect2.offset().top - 100;
@@ -95,4 +63,12 @@ $(function(){
         $("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
+    // slider by slick
+    $('.slider').slick({
+	    autoplay: true,
+	    autoplaySpeed: 3000,
+	    dots: true,
+	    adaptiveHeight: true,
+
+	});
 });
